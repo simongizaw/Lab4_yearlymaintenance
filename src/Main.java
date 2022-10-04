f20c97b9a439236e7065c17579cf2be585c72350
simon <simongizaw@gmail.com>
2022-10-04 18:45:12 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+
+        double costSummer = 150.34;
+        double costWinter = 120.45;
+        double costFall = 139.45;
+        double costSpring = 344.56;
+        double totalCost = (costSpring + costFall + costWinter + costSummer);
+
+
+        System.out.println( " Your spending for the summer was " + costSummer);
+        System.out.println( " Your spending for the fall was " + costFall);
+        System.out.println( " Your spending for the Winter was " + costWinter);
+        System.out.println( " Your spending for the spring was " + costSpring + " and your total spending was " + totalCost);
+
+
+    }
+}
